d5d1369e37b8024d2986d95943133287d1d0cec0
Kammin <ibayka@gmail.com>
2017-09-21 23:46:13 +0300

    Initial commit

---
+++ app/src/main/java/kamin/com/objviewer/MainActivity.java
@@ -0,0 +1,49 @@
+package kamin.com.objviewer;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private GLSurfaceView glSurfaceView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!supportES3()) {
+            Toast.makeText(this, "OpenGl ES 3.0 is not supported", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setEGLContextClientVersion(3);
+        glSurfaceView.setRenderer(new OpenGLRenderer());
+        setContentView(glSurfaceView);
+    }
+
+    private boolean supportES3() {
+        ActivityManager activityManager =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return (configurationInfo.reqGlEsVersion >= 0x30000);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+}
---
+++ app/src/main/java/kamin/com/objviewer/OpenGLRenderer.java
@@ -0,0 +1,25 @@
+package kamin.com.objviewer;
+
+import android.opengl.GLES30;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class OpenGLRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES30.glClearColor(0.8f, 0.8f, 0.5f, 1f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+        GLES30.glViewport(0, 0, i, i1);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+    }
+}
