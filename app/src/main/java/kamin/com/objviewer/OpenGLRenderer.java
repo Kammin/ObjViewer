5bc69e171fbf979aa3aae05810a030e112c4ec12
Kammin <ibayka@gmail.com>
2017-09-26 07:02:39 +0300

    4

--- app/src/main/java/kamin/com/objviewer/OpenGLRenderer.java
+++ app/src/main/java/kamin/com/objviewer/OpenGLRenderer.java
@@ -8,6 +8,8 @@ import android.os.SystemClock;
 import android.util.Log;
 
 import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -26,7 +28,7 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     Loader loader;
 
     private int programId;
-    private FloatBuffer vertexData;
+    private FloatBuffer vertexData, vertexAxisData;
 
     private int uColorLocation;
     private int aPositionLocation;
@@ -43,66 +45,13 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         loader = new Loader(context);
         prepareData();
     }
-    @Override
-    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-        GLES30.glClearColor(0f, 0f, 0f, 1f);
-        GLES30.glEnable(GLES30.GL_DEPTH_TEST);
-        programId = Utils.createProgram(context, R.raw.vertex_shader, R.raw.fragment_shader);
-        glUseProgram(programId);
-        prepareData();
-        bindData();
-    }
 
-    @Override
-    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
-        GLES30.glViewport(0, 0, i, i1);
-        createProjectionMatrix(i, i1);
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl10) {
-        createViewMatrix();
-        bindMatrix();
-        GLES30.glClear(GLES30.GL_DEPTH_BUFFER_BIT |GLES30.GL_COLOR_BUFFER_BIT);
-
-        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 3);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 1, 4);
-
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 2, 5);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 3, 6);
-
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 4, 7);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 5, 8);
-
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 6, 9);
-        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 7, 10);
-
-/*
-        // оси
-        GLES30.glLineWidth(1);
-
-        glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_LINES, 12, 2);
-
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_LINES, 14, 2);
-
-        glUniform4f(uColorLocation, 1.0f, 0.5f, 0.0f, 1.0f);
-        GLES30.glDrawArrays(GLES30.GL_LINES, 16, 2);
-*/
-
-    }
 
     private void bindData() {
         aPositionLocation = glGetAttribLocation(programId, "a_Position");
         vertexData.position(0);
         glVertexAttribPointer(aPositionLocation, 3, GLES30.GL_FLOAT,
-                true, 0, vertexData);
-        glEnableVertexAttribArray(aPositionLocation);
+                false, 0, vertexData);
 
         // цвет
         uColorLocation = glGetUniformLocation(programId, "u_Color");
@@ -112,50 +61,24 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     }
 
     private void prepareData() {
-/*        float s = 0.4f;
-        float d = 0.9f;
         float l = 3;
-
-        float[] vertices = {
-
-                // первый треугольник
-                -2*s, -s, d,
-                2*s, -s, d,
-                0, s, d,
-
-                // второй треугольник
-                -2*s, -s, -d,
-                2*s, -s, -d,
-                0, s, -d,
-
-                // третий треугольник
-                d, -s, -2*s,
-                d, -s, 2*s,
-                d, s, 0,
-
-                // четвертый треугольник
-                -d, -s, -2*s,
-                -d, -s, 2*s,
-                -d, s, 0,
-
+        float[] verticesAxis = {
                 // ось X
-                -l, 0,0,
-                l,0,0,
-
+                -l, 0, 0,
+                l, 0, 0,
                 // ось Y
-                0,-l,0,
-                0,l,0,
-
+                0, -l, 0,
+                0, l, 0,
                 // ось Z
-                0,0,-l,
-                0,0,l,
+                0, 0, -l,
+                0, 0, l,
         };
-
-        vertexData = ByteBuffer
-                .allocateDirect(vertices.length * 4)
+        vertexAxisData = ByteBuffer
+                .allocateDirect(verticesAxis.length * 4)
                 .order(ByteOrder.nativeOrder())
                 .asFloatBuffer();
-        vertexData.put(vertices);*/
+        vertexAxisData.put(verticesAxis);
+
         File file = loader.parse(R.raw.cube)[0];
         vertexData = loader.LoadBuff(file);
         for(int i =0;i<vertexData.capacity();i++){
@@ -164,7 +87,7 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     }
 
-    private void createViewMatrix() {
+    public void createViewMatrix() {
         // точка положения камеры
         float coef = (float)(SystemClock.uptimeMillis() % 10000) / 10000;
         float angle = coef  *  2 * 3.1415926f;
@@ -211,5 +134,46 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
     private void bindMatrix() {
         Matrix.multiplyMM(resultMatrix, 0, projectionMatrix, 0, cameraMatrix, 0);
         glUniformMatrix4fv(uMatrixLocation, 1, false, resultMatrix, 0);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES30.glClearColor(0f, 0f, 0f, 1f);
+        GLES30.glEnable(GLES30.GL_DEPTH_TEST);
+        programId = Utils.createProgram(context, R.raw.vertex_shader, R.raw.fragment_shader);
+        glUseProgram(programId);
+        prepareData();
+        bindData();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+        GLES30.glViewport(0, 0, i, i1);
+        createProjectionMatrix(i, i1);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        createViewMatrix();
+        bindMatrix();
+        GLES30.glClear(GLES30.GL_DEPTH_BUFFER_BIT |GLES30.GL_COLOR_BUFFER_BIT);
+
+        glEnableVertexAttribArray(aPositionLocation);
+        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 3);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 1, 4);
+
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 2, 5);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 3, 6);
+
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 4, 7);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 5, 8);
+
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 6, 9);
+        GLES30.glDrawArrays(GLES30.GL_TRIANGLES, 7, 10);
+
     }
 }
